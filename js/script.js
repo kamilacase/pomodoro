8f9603fcf3c38d8cba7a5a0db1dd4f3291136f1d
kamilacase <xakdxshooter@live.fr>
2018-06-28 12:28:48 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-    ('#addTask').click
+// $(document).ready(function() {
+//     ('#addTask').click
+// })
+$(document).ready(function () {
+    $('#addtask').on("click", function () {
+        $('.todo-list-div ul').append(
+            "<li>" + $("#drag").val() + " <a><<i class="fas fa-user-check"></i></a></li>";
+        )
+    })
+
 })
